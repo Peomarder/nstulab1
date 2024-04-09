6a785db290dca2b0ceeee05c12487218562ba461
Peomarder <36908523+Peomarder@users.noreply.github.com>
2024-04-09 21:55:36 +0700

    added main function prototype

---
+++ main.cpp
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+int main(){
+	unsigned char ChoiceOption = '0';
+	cout << "Choose shape description sub-programm: \n\t 1. Rectangle \n\t 2. Triangle \n\t 3. Trapezoid \n\t 4. Circle\n\n";
+	
+	cin >> ChoiceOption;
+	
+	switch (ChoiceOption)
+	{
+	case '1' : 
+	{
+		
+		break;
+	}
+	case '2' :
+	{
+		
+		break;
+	}
+	case '3' :
+	{
+		
+		break;
+	}
+	case '4' :
+	{
+		
+		break;
+	}
+	default: return -1;
+	}
+	return 0;
+}
