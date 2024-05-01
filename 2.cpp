01781a003aa002fb6d5af13425f337cf2cb1d620
Peomarder <36908523+Peomarder@users.noreply.github.com>
2024-05-01 18:36:49 +0700

    Update and rename main.cpp to 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -10,8 +10,9 @@ int TriangleP(int a, int b, int c){
 	return a+b+c;
 }
 
-int TriangleS(int a, int b, int c){ 
-int perimeter=TriangleP(a,b,c);
+double TriangleS(int a, int b, int c){ 
+double perimeter=TriangleP(a,b,c)/2.0;
+
 return sqrt(perimeter*(perimeter-a)*(perimeter-b)*(perimeter-c));
 	
 }
@@ -22,43 +23,18 @@ double TriangleM(int a){
 
 int main(){
 	unsigned char ChoiceOption = '0';
-	cout << "Choose shape description sub-programm: \n\t 1. Rectangle \n\t 2. Triangle \n\t 3. Trapezoid \n\t 4. Circle\n\n";
-	
-int a,b,c;
-	cin >> ChoiceOption;
-	
-	switch (ChoiceOption)
-	{
-	case '1' : 
-	{
-		
-		break;
-	}
-	case '2' :
-	{
-		
-cin>>a>>b>>c;
+	cout << "Enter the 3 sides of a triangle \n\n";
+	int a,b,c;
+	cin>>a>>b>>c;
 		if(((a+b)<c)||((a+c)<b)||((b+c)<a)){
 			cout<<"provided sides do not form a triangle";
-			break;
+			return -1;
 			}
 		
 		cout<<"perimeter: "<<TriangleP(a,b,c)<<"\n";
 		cout<<"area: "<<TriangleS(a,b,c)<<endl;
 		cout<<"corresponding midle lines are: "<<TriangleM(a)<<" "<<TriangleM(b)<<" "<<TriangleM(c)<<endl;
-		break;
-	}
-	case '3' :
-	{
 		
-		break;
-	}
-	case '4' :
-	{
-		
-		break;
-	}
-	default: return -1;
-	}
+
 	return 0;
 }
