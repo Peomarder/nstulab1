8dbd1c24bb1b6d4ef998151ea0876d282601fc31
Peomarder <36908523+Peomarder@users.noreply.github.com>
2024-05-03 13:53:58 +0700

    3.cpp

--- 3.cpp
+++ 3.cpp
@@ -14,16 +14,25 @@ int main(){
         case 1:
             cout << "a, b, c, d = ";
             cin >> a >> b >> c >> d;
+            if ((a <= 0) || (b <= 0) || (c <= 0) || (d <= 0)){
+        cout << "Uncorrect data" << endl;
+        return 1;}
             cout << perimeter(a, b, c , d);
             break;
         case 2:
             cout << "a, b, h = ";
             cin >> a >> b >> h;
+            if ((a <= 0) || (b <= 0) || (h <= 0)){
+        cout << "Uncorrect data" << endl;
+        return 1;}
             cout << area(a, b, h);
             break;
         case 3:
             cout << "a, b = ";
             cin >> a >> b;
+            if ((a <= 0) || (b <= 0)){
+        cout << "Uncorrect data" << endl;
+        return 1;}
             cout << middleLine(a, b);
             break;
         default:
@@ -31,7 +40,6 @@ int main(){
             break;
     }
 }
-
 int perimeter(int a, int b, int c, int d){
     return a+b+c+d;
 }
